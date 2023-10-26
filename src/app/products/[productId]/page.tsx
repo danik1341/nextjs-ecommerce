b13726ecd17b6944334706005126bdb9c474be16
danik1341 <danik1341@gmail.com>
2023-10-26 15:23:37 +0300

    products page

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,67 @@
+import PriceTag from "@/components/PriceTag";
+import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React, { cache } from "react";
+import AddToCartButton from "./AddToCartButton";
+import { incrementProductQuantity } from "./actions";
+
+interface ProductPageProps {
+  params: {
+    productId: string;
+  };
+}
+
+const getProduct = cache(async (productId: string) => {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { productId },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(productId);
+
+  return {
+    title: `${product.name} - Flowmazon`,
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { productId },
+}: ProductPageProps) {
+  const product = await getProduct(productId);
+
+  return (
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+        <AddToCartButton
+          productId={product.id}
+          incrementProductQuantity={incrementProductQuantity}
+        />
+      </div>
+    </div>
+  );
+}
